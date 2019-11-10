9b2885591d3d32d5ce41ab10b3c96d20abdaa8f5
Tchangang <tchangang.boris@gmail.com>
2019-11-10 13:00:24 +0100

    remove useless text

--- src/index.test.ts
+++ src/index.test.ts
@@ -25,18 +25,18 @@ test('Test JenaTester', () => {
                expect(jenaTest.steps[0].tests[1].success).toBe(false);
            }
            const xml = jenaTest.steps[0].getXml();
-           const expectedXml = `<testsuite id="firstStep" name="New function" tests="2" failures="1" time="0">
-                <testcase id="id1" name="1 should be 1" time="0">
-               </testcase>
-               <testcase id="id2" name="1 should &quot; be 2" time="0">
-               <failure>
-                   Test failed: 1 should be 2
-               expected: 2
-               received: 1
-               </failure>
-               </testcase>
-               </testsuite>
-           `;
+           // const expectedXml = `<testsuite id="firstStep" name="New function" tests="2" failures="1" time="0">
+           //      <testcase id="id1" name="1 should be 1" time="0">
+           //     </testcase>
+           //     <testcase id="id2" name="1 should &quot; be 2" time="0">
+           //     <failure>
+           //         Test failed: 1 should be 2
+           //     expected: 2
+           //     received: 1
+           //     </failure>
+           //     </testcase>
+           //     </testsuite>
+           // `;
            // expect(xml.replace(/\s/g, '')).toBe(expectedXml.replace(/\s/g, ''));
            expect(xml.includes('&quot;')).toBe(true);
            await new Promise((resolve) => {
